83f602aef231cabc5e2101c00b4fc677c05f9396
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-03-12 00:50:56 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,10 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function(currentTime) {
-       localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime)); 
-});
+player.on('timeupdate', throttle (function(currentTime) {
+   localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds) 
+)}, 1000));
+
 
 const time = localStorage.getItem('videoplayer-current-time');
 //console.log(time);
@@ -14,7 +15,7 @@ const playStopTime = JSON.parse(time);
 //console.log(playStopTime);
 
 player.setCurrentTime(playStopTime.seconds||0).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
+    
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = JSON.parse (localStorage.getItem(STORAGE_KEY)) || {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(handleformData, 500));
+form.addEventListener('submit', handleFormSubmit);
+
+//-----------input------------------
+function handleformData (event) {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+    }
+//------------submit------------------
+function handleFormSubmit(event) {
+    event.preventDefault();// Забороняємо перезагружати сторінку
+    event.currentTarget.reset();//очищаємо введені дані з форми
+    localStorage.removeItem(STORAGE_KEY);  //очищаємо локальні дані
+    console.log(formData);
+    formData = {};    
+}
+
+function removePages () {
+    if (formData) {
+        let {email, message} = form.elements;
+        email.value = formData.email || '';
+        message.value = formData.message || '';
+    }
+}
+ removePages(); 
