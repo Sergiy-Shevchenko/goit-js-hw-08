c71dc07ec56a1eae88356f4b8935c0d8e42b7d2d
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-03-12 13:43:26 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,26 +4,9 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle (function(currentTime) {
-   localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds) 
-)}, 1000));
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time'))) 
+const saveCurrentTime = event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
-
-const time = localStorage.getItem('videoplayer-current-time');
-//console.log(time);
-const playStopTime = JSON.parse(time);
-//console.log(playStopTime);
-
-player.setCurrentTime(playStopTime.seconds||0).then(function(seconds) {
-    
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
